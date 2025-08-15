bce7bbdc8bdea6a0b7a4520bb685a26736e79151
Tejinderpal singh <146336906+tejinderpa@users.noreply.github.com>
2025-08-15 10:22:53 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,90 @@
+import os
 import streamlit as st
+import pickle
+import time
+import google.generativeai as genai
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import asyncio
+import nest_asyncio
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+nest_asyncio.apply()
+
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") or st.secrets.get("GOOGLE_API_KEY")
+
+if not GOOGLE_API_KEY:
+    st.error("Gemini API Key not found. Please set GEMINI_API_KEY in environment or secrets.")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+# file_path = "faiss_store_openai.pkl" # This is not needed anymore
+INDEX_DIR = "faiss_index"
+
+main_placeholder = st.empty()
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=GOOGLE_API_KEY)
+
+async def process_urls_and_create_index(urls, main_placeholder, INDEX_DIR, GOOGLE_API_KEY):
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001",
+        google_api_key=GOOGLE_API_KEY
+    )
+
+    vectorindex_google = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+
+    vectorindex_google.save_local(INDEX_DIR)
+    main_placeholder.text("Index saved locally! ✅")
+
+if process_url_clicked:
+    asyncio.run(process_urls_and_create_index(urls, main_placeholder, INDEX_DIR, GOOGLE_API_KEY))
+
+
+# Load the vector index if the directory exists
+if os.path.exists(INDEX_DIR):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
+    vectorstore = FAISS.load_local(INDEX_DIR, embeddings, allow_dangerous_deserialization=True)
+    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+
+    query = main_placeholder.text_input("Question: ")
+    if query:
+        result = chain({"question": query}, return_only_outputs=True)
+        # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+        st.header("Answer")
+        st.write(result["answer"])
+
+        # Display sources, if available
+        sources = result.get("sources", "")
+        if sources:
+            st.subheader("Sources:")
+            sources_list = sources.split("\n")  # Split the sources by newline
+            for source in sources_list:
+                st.write(source)
+else:
+    main_placeholder.text("Please process URLs to create the index.")
